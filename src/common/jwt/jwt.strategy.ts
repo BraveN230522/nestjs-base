6e1dfb80d5c32d30cbf409d60afb24b1ed62b0f7
Brave N <braven230522@gmail.com>
2023-01-16 14:22:17 +0700

    Initialize nestjs base

---
+++ src/common/jwt/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Role } from '../../enums';
+import { JwtPayload } from '../../interfaces';
+import { Admin } from '../../modules/admin/admin.entity';
+import { AdminRepository } from '../../modules/admin/admin.repository';
+import { AuthService } from '../../modules/auth/auth.service';
+import { User } from '../../modules/users/users.entity';
+import { UsersRepository } from '../../modules/users/users.repository';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super({
+      secretOrKey: 'top',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User | Admin> {
+    const auth = this.authService.validate(payload);
+    if (!auth) throw new UnauthorizedException();
+
+    return auth;
+  }
+}
